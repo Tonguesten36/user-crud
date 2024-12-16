2dd252cf3c692c13270514bc4d42913a39a353e2
Tonguesten36 <tung@tungsoft.net>
2024-12-16 11:20:02 +0700

    Initial commit

---
+++ src/test/java/com/toshiba/intern/usercrud/TestUserCrudApplication.java
@@ -0,0 +1,11 @@
+package com.toshiba.intern.usercrud;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestUserCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(UserCrudApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
